b2ffb8588d2d5be016b3d8e7c0c93ed76854633a
wangyonghao <wangyonghao@rongcloud.cn>
2020-07-29 16:31:12 +0800

    fix: update imsdk 3.0.5

--- im.js
+++ im.js
@@ -1,58 +1,19 @@
-function reconnect() {
-  var callback = {
-    onSuccess: function (userId) {
-      console.log('reconnect success. ' + userId);
-    },
-    onTokenIncorrect: function () {
-      console.log('token 无效');
-    },
-    onError: function (errorCode) {
-      reconnect();
-    }
-  };
-  var config = {
-    auto: true,
-    url: 'cdn.ronghub.com/RongIMLib-2.2.6.min.js?d=' + Date.now(),
-    rate: [100, 1000, 3000, 6000, 10000]
-  };
-  RongIMClient.reconnect(callback, config);
-}
-
 function initIM(params, callbacks) {
     var appkey = params.appkey;
     var token = params.token;
-    RongIMClient.init(appkey, null);
-    RongIMClient.setConnectionStatusListener({
-      onChanged: function (status) {
-        switch (status) {
-          case RongIMLib.ConnectionStatus.CONNECTING:
-            break;
-          case RongIMLib.ConnectionStatus.DISCONNECTED:
-            callbacks.disconnectd(status);
-          break;
-          case RongIMLib.ConnectionStatus.KICKED_OFFLINE_BY_OTHER_CLIENT:
-          case RongIMLib.ConnectionStatus.DOMAIN_INCORRECT:
-          case RongIMLib.ConnectionStatus.NETWORK_UNAVAILABLE:
-            reconnect();
-            // callbacks.error(status);
-            break;
-        }
+    var im = RongIMLib.init({
+      appkey
+    });
+    im.watch({
+      status: function (event) {
+        console.log('status changed', event);
       }
     });
-    RongIMClient.setOnReceiveMessageListener({
-      onReceived: function (message) {
-        console.log(message);
-      }
-    });
-    RongIMClient.connect(token, {
-      onSuccess: function (userId) {
-        callbacks.connected(RongIMClient.getInstance(), { id: userId });
-      },
-      onTokenIncorrect: function () {
-        callbacks.onTokenIncorrect()
-      },
-      onError: function (code) {
-        callbacks.disconnectd(status);
-      }
+    im.connect({
+      token
+    }).then(({ id }) => {
+      callbacks.connected(im, { id });
+    }).catch(({ code }) => {
+      callbacks.disconnectd(code);
     });
   }
