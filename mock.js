2ba8cb3a22156998076230c2143937146795d4b3
wangyonghao <wangyonghao@rongcloud.cn>
2020-07-29 14:26:22 +0800

    feat: init demo

---
+++ mock.js
@@ -0,0 +1,23 @@
+var App = {
+  appkey: 'n19jmcy59f1q9',
+  users: [
+    {
+      token: 'Kn2p4uokgY5AZOFVsKTbKq+YsUIoF3ojin3K277sfOnEb+B6ZpahsTOCVisdS43pwz7SnsSF0xxiLfygEojZP7ywLi39+nOPq12llTIt1oc='
+    },
+    {
+      token: '5JQlp5czM31GNl99DOZyI3xpRjANxKgfakOnYLFljI+TMvOF0hGaVtR1n9Qp4baLgKBGsyl3w5j4gAWBbNZ3nOKrvnVo8Ldl'
+    },
+    // {
+    //   token: 'CRCFskU1gksWKPQPrBv4ZOIrqBpg5Fa5pbHAFA8kLoa344ZzkijEYg==@h4mx.cn.rongnav.com;h4mx.cn.rongcfg.com'
+    // },
+    // {
+    //   token: 'CRCFskU1gkuOKssJpmS+ceIrqBpg5Fa5XGXNNieidZ3LHdQNxUf2YQ==@h4mx.cn.rongnav.com;h4mx.cn.rongcfg.com'
+    // },
+    // {
+    //   token: 'CRCFskU1gkvQ3HAXZE/UCuIrqBpg5Fa53rnOGN6aracaCK3mMrAdZA==@h4mx.cn.rongnav.com;h4mx.cn.rongcfg.com'
+    // },
+    // {
+    //   token: 'CRCFskU1gkudM8wLN6Qj9OIrqBpg5Fa5of0xknfb0zdPJC3MkE3QaA==@h4mx.cn.rongnav.com;h4mx.cn.rongcfg.com'
+    // }
+  ]
+};
